840c0f9309d1c452a15c18e69efcf7df7e67c216
ScottCheewah <scott.cheewah@yahoo.com>
2023-01-27 15:46:45 -0500

    Player Rotation
    -Player rotates to face the mouse

--- SAFE - Capstone Project/Assets/Shtuff/PlayerAttack.cs
+++ SAFE - Capstone Project/Assets/Shtuff/PlayerAttack.cs
@@ -10,7 +10,8 @@ public class PlayerAttack : MonoBehaviour
     // Vector3 rotation;
     void Start()
     {
-        weapon = gameObject.transform.GetChild(1).gameObject;
+        // weapon = gameObject.transform.GetChild(1).gameObject;
+        weapon = gameObject;
         player = GameObject.Find("Player");
     }
 
@@ -19,8 +20,8 @@ public class PlayerAttack : MonoBehaviour
         screenPosition = Input.mousePosition;
         worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
         zeroPoint = new Vector2(worldPosition.x - transform.position.x, worldPosition.y - transform.position.y);
-        if(worldPosition.x > transform.position.x) weapon.transform.rotation = Quaternion.Euler(((Mathf.Atan(zeroPoint.y/zeroPoint.x) * (180 / Mathf.PI)) + 180),-90, 0);
-        else weapon.transform.rotation = Quaternion.Euler((Mathf.Atan(zeroPoint.y/zeroPoint.x) * (180 / Mathf.PI)),-90, 0);
+        if(worldPosition.x > transform.position.x) weapon.transform.rotation = Quaternion.Euler(0, 0, (Mathf.Atan(zeroPoint.y/zeroPoint.x) * (180 / Mathf.PI)) - 90);
+        else weapon.transform.rotation = Quaternion.Euler(0, 0, ((Mathf.Atan(zeroPoint.y/zeroPoint.x) * (180 / Mathf.PI)) + 90));
         
     }
 }
